a808c8ae02b5d49b5af5d2034d157fcf490c9543
ranupatidar05 <ranupatidar4589@gmail.com>
2023-07-23 13:29:23 +0530

    new

---
+++ js/custom3.1.js
@@ -0,0 +1,18 @@
+//console.log('zig-zag assiment');
+// \<- is called escape character
+console.log("/");
+//console.log("/\";)
+console.log("/\\");
+console.log("/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\");
+
+console.log('india\'s'+" grip"+' in'+' the'+' world'+' test'+' championship'+' final'+' is'+' slipping');
+
+console.log("hello \"okLabs student\" ");
+
+console.log("ranu"+20); // string + number
+console.log(20+"ranu"); // number + string 
+console.log(30+20); // number + number + additon operator
+console.log("20"+"30");// string + string concatination operator
+
+
+ 
